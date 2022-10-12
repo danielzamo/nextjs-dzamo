b3ade1a5eed4b59972fc871c10e309c56bd7173d
Daniel Oscar Zamo <daniel.oscar.zamo@gmail.com>
2022-10-12 23:30:38 +0200

    update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,4 @@
 import Head from 'next/head'
-import About from '../components/About'
-import Contact from '../components/Contact'
 import Main from '../components/Main'
 import Projects from '../components/Projects'
 import Skills from '../components/Skills'
@@ -10,15 +8,13 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <title>Clint | Front-End Developer</title>
-        <meta name="description" content="I’m a front-end web developer specializing in building (and occasionally designing) exceptional digital experiences." />
+        <title>Dzamo | static content</title>
+        <meta name="description" content="Ansible, RHEL + derivate, Debian + derivate, KVM, Docker" />
         <link rel="icon" href="/fav.png" />
       </Head>
     <Main />
-    <About />
     <Skills />
     <Projects />
-    <Contact />
     </div>
   )
 }
