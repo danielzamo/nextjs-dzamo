9dc649a690816396d6f625a301a15e18989a5be9
Daniel Oscar Zamo <daniel.oscar.zamo@gmail.com>
2022-10-23 00:40:33 +0200

    fix error in lamp-service.jsx

--- pages/lamp-service.jsx
+++ pages/lamp-service.jsx
@@ -1,6 +1,6 @@
 import Image from 'next/image';
 import React from 'react';
-import lampService from '../public/assets/projects/lamp-service.jpg';
+import lampService from '../public/projects/lamp-service.jpg';
 import { RiRadioButtonFill } from 'react-icons/ri';
 import Link from 'next/link';
 
