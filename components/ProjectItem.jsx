e237e1a3662c663661ab51514b5ccc3503d752bb
Daniel Oscar Zamo <daniel.oscar.zamo@gmail.com>
2022-10-22 23:23:11 +0200

    update files

--- components/ProjectItem.jsx
+++ components/ProjectItem.jsx
@@ -4,8 +4,7 @@ import React from 'react'
 
 const ProjectItem = ({title, backgroundImg, tech, projectUrl, alt}) => {
   return (
-    <div className='relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl group hover:bg-gradient-to-r from-[#297CFC] to-[#709dff]'> 
-{/**    <div className='relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl group hover:bg-gradient-to-r from-[#297CFC] to-[#709dff]'>  */}
+    <div className='relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl group hover:bg-gradient-to-r from-[#297CFC] to-[#709dff]'>  
 {/**    <Image className='rounded-xl group-hover:opacity-10' src={backgroundImg} width='54px' height='54px' alt='/' />  */}
     <Image 
       className='rounded-xl group-hover:opacity-10' 
