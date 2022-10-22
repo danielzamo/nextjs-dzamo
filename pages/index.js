cd4f9b3f99fba7f49bdd6f6e04ca881d16d8fc5e
Daniel Oscar Zamo <daniel.oscar.zamo@gmail.com>
2022-10-22 23:32:06 +0200

    update index.js

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,6 @@ import Navbar from "../components/Navbar";
 import Main from "../components/Main";
 import Skills from "../components/Skills";
 import Projects from "../components/Projects";
-
 /*
 import About from "../components/About";
 import Contact from "../components/Contact";
